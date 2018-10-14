51701045f8e02fc389de5b769260024ce0b3ee3f
VasiliiChernobai <vasilii.chernobai@gmail.com>
2018-10-14 16:11:11 +0300

    Initial commit

---
+++ src/main/java/com/vasili/chernobai/fileManager/FileManagerApplication.java
@@ -0,0 +1,12 @@
+package com.vasili.chernobai.fileManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManagerApplication.class, args);
+	}
+}
